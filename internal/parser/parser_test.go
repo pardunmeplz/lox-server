f313d083aa1175ba4f3fd61287bc6f9352ce0541
Parth Dundh <dundh.parth@gmail.com>
2026-04-05 17:34:37 +0530

    Parser test changes

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -1,7 +1,9 @@
 package parser
 
 import (
+	"bytes"
 	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -12,7 +14,7 @@ func initParser() ParserState {
 
 func TestPositiveFlow(t *testing.T) {
 	parser := initParser()
-	req := []byte("Content-Length: 129\r\n\r\n{\"jsonrpc\": \"2.0\",\"id\": 0,\"result\": {\"capabilities\": {\"textDocumentSync\": 1,\"completionProvider\": { \"resolveProvider\": true }}}}")
+	req := []byte("Content-Length: 128\r\n\r\n{\"jsonrpc\": \"2.0\",\"id\": 0,\"result\": {\"capabilities\": {\"textDocumentSync\": 1,\"completionProvider\": { \"resolveProvider\": true }}}}")
 
 	consumed, err := parse(req, &parser)
 	if err != nil {
@@ -24,4 +26,46 @@ func TestPositiveFlow(t *testing.T) {
 		t.Fatal(fmt.Sprintf("invalid consumed value %d instead of %d", consumed, len(req)))
 	}
 
+	if parser.Request.Headers["Content-Length"] != "128" {
+		t.Fatal(fmt.Sprintf("invalid content length value %s", parser.Request.Headers["Content-Length"]))
+	}
+
+	expectedContent := []byte("{\"jsonrpc\": \"2.0\",\"id\": 0,\"result\": {\"capabilities\": {\"textDocumentSync\": 1,\"completionProvider\": { \"resolveProvider\": true }}}}")
+	if !bytes.Equal(parser.Request.Content, expectedContent) {
+		t.Fatal(fmt.Sprintf("invalid content %s", string(parser.Request.Content)))
+	}
+}
+
+func TestContentLength(t *testing.T) {
+	// missin content length check
+	parser := initParser()
+	req := []byte("\r\n\r\n{\"jsonrpc\": \"2.0\",\"id\": 0,\"result\": {\"capabilities\": {\"textDocumentSync\": 1,\"completionProvider\": { \"resolveProvider\": true }}}}")
+
+	_, err := parse(req, &parser)
+	if !strings.Contains(err.Error(), MISSING_CONTENT_LENGTH) {
+		t.Fatal("Error expected, got %w", err)
+	}
+
+	// invalid content length check
+	parser = initParser()
+	req = []byte("Content-Length: 12A\r\n\r\n{\"jsonrpc\": \"2.0\",\"id\": 0,\"result\": {\"capabilities\": {\"textDocumentSync\": 1,\"completionProvider\": { \"resolveProvider\": true }}}}")
+
+	_, err = parse(req, &parser)
+	if !strings.Contains(err.Error(), "Invalid Content-Length") {
+		t.Fatal("Error expected, got %w", err)
+	}
+
+	// content length smaller than incoming request bytes, check if content length is honored correctly
+	parser = initParser()
+	req = []byte("Content-Length: 12\r\n\r\n{\"jsonrpc\": \"2.0\",\"id\": 0,\"result\": {\"capabilities\": {\"textDocumentSync\": 1,\"completionProvider\": { \"resolveProvider\": true }}}}")
+
+	_, err = parse(req, &parser)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedContent := []byte("{\"jsonrpc\": ")
+	if !bytes.Equal(parser.Request.Content, expectedContent) {
+		t.Fatal(fmt.Sprintf("invalid content %s", string(parser.Request.Content)))
+	}
 }
